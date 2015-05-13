cff29d5d84f1b6a0be524a01fc3d9ec1ff16a456
Shannon Code <genecyber@gmail.com>
2015-05-13 13:53:16 -0400

    minor cit fix

--- continuous.js
+++ continuous.js
@@ -2,12 +2,14 @@ var express = require('express')
 var basicAuth = require('http-auth')
 
 var app = express()
-var authMiddleware = basicAuth.connect(basic)
+
 var basic = basicAuth.basic({
     realm: 'Admin Stuff'
 }, function(username, password, callback) {
     callback(username == 'admin' && password == 'password');
 })
+
+var authMiddleware = basicAuth.connect(basic)
 
 app.get('/deploy',authMiddleware,function(req,res){
     res.json({status:"deploying"})
