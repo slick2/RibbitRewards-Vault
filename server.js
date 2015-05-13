00a674c942d03b0a725ce5c1ce4d0b611d1340bf
Shannon Code <genecyber@gmail.com>
2015-05-13 14:34:49 -0400

    fixing server

--- server.js
+++ server.js
@@ -6,6 +6,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname ));
+app.use(express.static("/root/RibbitRewards-Vault/" ));
 
 app.listen(process.env.PORT || 3006);
